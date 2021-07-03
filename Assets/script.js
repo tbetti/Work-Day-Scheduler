93e85cd5c6bab62c5b75a15e95956109dc6c4d9e
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-03 11:53:18 -0400

    Adjusted some styles, made variable names more descriptive

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 // Constants and global variables
-var START_HOUR = 8;
+var START_HOUR = 9;
 var TOTAL_WORKDAY = 8;
-var hours = moment().format("h");
+var currentHour = moment().format("h");
 
 // Display current date
 var currentDate = moment().format("dddd, MMMM Do");
@@ -10,34 +10,36 @@ $("#currentDay").text(currentDate);
 // Create multiple time blocks:
 var container = $(".container");
 for(i=START_HOUR; i < TOTAL_WORKDAY+START_HOUR; i++){
-    var row = $("<div>").attr("class", "row hour time-block");
-    var time = $("<div>").attr("class", "col-2 column");
-    var inputColumn = $("<div>").attr("class", "col-8 column");
-    var saveBtn = $("<div>").attr("class", "col-2 column saveBtn");
+    var row = $("<div>").attr("class", "row time-block");
+    var hour = $("<div>").attr("class", "col-1 column hour");
+    var inputColumn = $("<div>").attr("class", "col-10 column");
+    var saveBtn = $("<div>").attr("class", "col-1 column saveBtn");
     var textArea = $("<textarea>");
+    var iBtn = $("<i>");
 
     container.append(row);
-    row.append(time);
+    row.append(hour);
     row.append(inputColumn);
     inputColumn.append(textArea);
     row.append(saveBtn);
+    saveBtn.append(iBtn); // did not really do anything on styling side
 
     // display time
     if (i<12)
     {
-        time.text(i + " AM");
+        hour.text(i + " AM");
     } else if (i===12){
-        time.text(i + "PM");
+        hour.text(i + "PM");
     }
     else {
-        var analog = i - 12;
-        time.text(analog + " PM");
+        var twelveHour = i - 12;
+        hour.text(twelveHour + " PM");
     }
 
     // change row colors based on current time
-    if(hours==i){
+    if(currentHour==i){
         inputColumn.addClass("present");
-    } else if (hours>i){
+    } else if (currentHour>i){
         inputColumn.addClass("past");
     } else{
         inputColumn.addClass("future");
